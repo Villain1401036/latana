9a52589ad38e76fe1666e2921ded14f98476978b
Villain1401036 <42384322+Villain1401036@users.noreply.github.com>
2022-09-08 22:01:14 +0530

    .

--- modules/utils.py
+++ modules/utils.py
@@ -69,7 +69,9 @@ def transformdata_raw(src_file,destfolder='/home/rahul/reddit/posts_transformed/
             return float(x)
 
     try:
-        df = pd.read_csv(src_file,chunksize=300000,dtype=dtype ) 
+        df = pd.read_csv(src_file,chunksize=300000,dtype=dtype)
+
+
         
         count_file = 1
         for data in df:
@@ -82,8 +84,12 @@ def transformdata_raw(src_file,destfolder='/home/rahul/reddit/posts_transformed/
             data["over_18"] = data["over_18"].fillna(True)
             #remove data where there is nan in id or subreddit_id
 
-            data["selftext"] = data["selftext"].replace("\n","" ).replace("\t","").replace("\r").replace("")
-            data["title"] = data["title"].replace("\n","" ).replace("\t","").replace("\r")
+
+
+            data["selftext"] = data["selftext"].replace("\n","" ).replace("\t","").replace("\r").replace("\r","").replace("|",",")
+            data["title"] = data["title"].replace("\n","" ).replace("\t","").replace("\r","").replace("|",",")
+
+
 
 
             data = data.dropna(subset=['id', 'subreddit_id'])
